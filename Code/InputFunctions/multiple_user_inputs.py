792cb2d5dd77516a23049a332bb46382934b6141
SALADI VEERA VENKATA RAJU <svvraju222@gmail.com>
2022-08-04 15:24:46 +0530

    added sets in notebook and reading userinput .py files

---
+++ Code/InputFunctions/multiple_user_inputs.py
@@ -0,0 +1,17 @@
+'''
+a = input()
+b = input()
+print('value of a is ', a)
+print('value of b is ', b)
+'''
+
+'''
+a = []
+for i in range(0, 5):
+    a.append(input("Enter a value "))
+print(a)
+'''
+
+a = input()
+b = a.split(' ')
+print("The value is ", b)
